813011b18ca1bcbc1598539f607a2d0d1c99fa6d
Vladimir Tyalo <bobtial@inbox.ru>
2016-04-13 01:40:59 +0400

    set browsersync setup

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,35 @@
 (function () {
   "use strict";
   var gulp = require('gulp');
+  var browserSync = require('browser-sync').create();
+
 
   gulp.task('default', function () {
     console.log("Build Ok");
   });
+
+  gulp.task('watch', function(gulpCallback) {
+    browserSync.init({
+      // serve out of app/
+      server: './',
+      port: 9876,
+      // launch default browser as soon as server is up
+      open: true
+    }, function callback() {
+      gulp.watch('index.html', browserSync.reload);
+
+      // watch css and stream to BrowserSync when it changes
+      gulp.watch('css/**', function() {
+        // grab css files and send them into browserSync.stream
+        // this injects the css into the page
+        gulp.src('css/**')
+          .pipe(browserSync.stream());
+      });
+
+      // notify gulp that this task is done
+      gulpCallback();
+    });
+  });
+
 
 })();
