6211efbde7645a15185184e39e5af2613480bc13
Vladimir Tyalo <bobtial@inbox.ru>
2016-04-13 01:00:31 +0400

    install gulp for convenience

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by vladimir on 4/13/16.
+ */
+(function () {
+  "use strict";
+  var gulp = require('gulp');
+
+  gulp.task('default', function () {
+    console.log("Build Ok");
+  });
+
+})();
