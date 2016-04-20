0aa2d7f22db2ffe4e1f4f96b5500d9071d72d4ee
Vladimir Tyalo <bobtial@inbox.ru>
2016-04-21 02:47:27 +0400

    style index.html down to map

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,36 @@
   var gulp = require('gulp');
   var browserSync = require('browser-sync').create();
   var autoprefixer = require('gulp-autoprefixer');
+  var svgmin = require('gulp-svgmin');
+  var svgstore = require("gulp-svgstore");
+  var path = require("path");
+  var inject = require('gulp-inject');
+  var htmlhint = require("gulp-htmlhint");
+
+
+  gulp.task("lint", function () {
+    gulp.src("index.html")
+        .pipe(htmlhint())
+        .pipe(htmlhint.failReporter());
+  });
+
 
   gulp.task('default', ["watch"], function () {
     console.log("Build Ok");
   });
 
-
   gulp.task('watch', function (gulpCallback) {
     browserSync.init({
       // serve out of ./
-      server: './',
-      port: 9876,
-      // launch default browser as soon as server is up
+      server: './', port: 9876, // launch default browser as soon as server is up
       //open: true
     }, function callback() {
       gulp.watch(['index.html', 'catalog.html'], browserSync.reload);
       // watch css and stream to BrowserSync when it changes
-      gulp.watch('css/**', function () {
+      gulp.watch('css/**/*.css', function () {
         // grab css files and send them into browserSync.stream
         // this injects the css into the page
-        gulp.src('css/**')
-          .pipe(browserSync.stream());
+        gulp.src('css/**/*.css').pipe(browserSync.stream());
       });
 
       // notify gulp that this task is done
@@ -34,14 +43,57 @@
     });
   });
 
+  // prefix css with autoprefixer
   gulp.task('prefix', function () {
     return gulp.src('css/style.css')
-      .pipe(autoprefixer({
-        browsers: ['> 2% in ru', "ie 9-11", "last 2 versions" ],
-        cascade: false
-      }))
-      .pipe(gulp.dest('css'));
+               .pipe(autoprefixer({
+                 browsers: BROWSER_OPTIONS, cascade: false
+               }))
+               .pipe(gulp.dest('css'));
   });
+
+  gulp.task('svgmin', function () {
+    return gulp.src('./img/icons/*.svg')
+               .pipe(svgmin(function getOptions(file) {
+                 var prefix = path.basename(file.relative, path.extname(file.relative));
+
+                 SVGMIN_PLAGINS.push({
+                   cleanupIDs: {
+                     prefix: prefix + '-', minify: true
+                   }
+                 });
+                 return {
+                   plugins: SVGMIN_PLAGINS
+                 }
+               }))
+               .pipe(svgmin({
+                 js2svg: {
+                   pretty: true
+                 }
+               }))
+               .pipe(svgstore())
+               .pipe(gulp.dest('./img/sprite/'));
+  });
+
+
+  const BROWSER_OPTIONS = ['> 2% in ru', "ie 9-11", "last 2 versions"];
+  const SVGMIN_PLAGINS = [{
+    removeDoctype: true
+  }, {
+    removeComments: true
+  }, {
+    cleanupNumericValues: {
+      floatPrecision: 2
+    }
+  }, {
+    convertColors: {
+      names2hex: false, rgb2hex: false
+    }
+  }, {
+    collections: true
+  }, {
+    moveElemsAttrsToGroup: true
+  }];
 
 
 })();
